cd98f9bf4f910ef73133fc8a08dfef9aa7ea05eb
Nagarani <nagarani@agiratech.com>
2016-10-21 17:11:28 +0530

    pdf creating getting the info

--- excelsheet-maxmarks.rb
+++ excelsheet-maxmarks.rb
@@ -1,21 +1,40 @@
 require 'spreadsheet'
 
-class Exls
+module Sheet
+
   attr_accessor :a,:b
-  def self.marksheet a=Array.new,b=Array.new 
-  
-    @a=a
-    @b=b
-  
-    Spreadsheet.open('/home/agira/rubyprograms/programs/mark.xls') do |book|
-	  book.worksheet('Sheet1').each do |row,column|
-		puts row=>column
-		@a.push(row)
-		@b.push(column)
-      end
-    end
-    puts "#{@b.max} got hiighst marks among all"
-  end
+
+     def initialize a=Array.new,b=Array.new
+          @@a=a
+          @@b=b
+     end
 end
 
-Exls.marksheet
+
+class Exls
+  include Sheet
+  
+  def marksheet  
+      puts "subject=>marks"
+   Spreadsheet.open('/home/agira/rubyprograms/programs/mark.xls') do |book|
+	  book.worksheet('Sheet1').each do |row,column|
+		puts row=>column
+		@@a.push row
+		@@b.push column
+      end
+    end
+    
+   end
+end
+
+
+class Exlsh < Exls
+
+    def self.pri
+      puts "#{@@b.max} got hiighst marks among all"
+    end
+
+end
+
+Exlsh.new.marksheet
+Exlsh.pri
