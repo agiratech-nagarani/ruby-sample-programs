5dcd207737ac23e4c4d5a589ab50a20a2e0587a5
Nagarani <nagarani@agiratech.com>
2016-10-21 17:15:43 +0530

    matrix mul and sum

--- matrix-sum-mul.rb
+++ matrix-sum-mul.rb
@@ -1,13 +1,17 @@
 require 'matrix'
 
-class Matris
-  attr_accessor :a_matrix,:b_matrix
-
-      def initialize a_matrix = Matrix[[1,2],[1,2]],b_matrix=Matrix[[1,2],[1,2]]
+module Calculation
+attr_accessor :a_matrix,:b_matrix
+ def initialize a_matrix = Matrix[[2,2],[5,7]],b_matrix=Matrix[[1,2],[1,2]]
           @a_matrix = a_matrix
           @b_matrix=b_matrix
       end
 
+end
+
+class Matris
+    include Calculation
+  
       def add 
         @a_matrix+@b_matrix
       end
@@ -15,12 +19,22 @@ class Matris
       def mul
         @a_matrix*@b_matrix
       end
+      
+
+end
+
+class Matris1 < Matris
+
+      def sub
+        @a_matrix-@b_matrix
+      end
 end
 
 
-m1=Matris.new 
+m1=Matris1.new 
 puts "sum of two matrices #{m1.add}"
 puts "sum of two matrices #{m1.mul}"
+puts "sum of two matrices #{m1.sub}"
 
 m2=Matris.new
 
